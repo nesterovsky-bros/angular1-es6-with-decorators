704da7265b024fa5e3c7e34a8a2737c8c0cbf7c4
vnesterovsky <vladimir@nesterovsky-bros.com>
2016-09-08 13:34:35 +0300

    Fix typo.

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ var config =
     },
     angular: 
     {
-      hasExports: true, // this is to forses to assign to global require.
+      hasExports: true, // this is to forse to assign to global require.
       require:
       [
         [
